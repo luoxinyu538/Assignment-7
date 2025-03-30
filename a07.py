de4d812107ea7c883a59d8f6e7b5b0819fa352da
luoxinyu538 <luox7260@mylaurier.ca>
2025-03-30 16:17:43 -0400

    Update a07.py

--- a07.py
+++ a07.py
@@ -2,7 +2,6 @@ import dash
 from dash import dcc, html, Input, Output
 import plotly.graph_objs as go
 import pandas as pd
-server=app.server
 world_cup_finals = [
     {"year": 1930, "winner": "Uruguay", "runner_up": "Argentina"},
     {"year": 1934, "winner": "Italy", "runner_up": "Czechoslovakia"},
@@ -50,7 +49,7 @@ fig_choropleth.update_layout(
 )
 
 app = dash.Dash(__name__)
-
+server=app.server
 app.layout = html.Div([
     html.H1("FIFA World Cup Dashboard", style={'textAlign': 'center'}),
     html.Div([
